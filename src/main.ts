5b4790715109d4ced4ba4745f489ff81ace2b470
VIXI0 <mickycanela@gmail.com>
2019-08-02 20:01:41 -0400

    fixed vue the mask and working vue config idk why

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
-import axios from 'axios'
-import VueTheMask from 'vue-the-mask'
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import '@mdi/font/css/materialdesignicons.css';
+import axios from 'axios';
+const VueTheMask = require('vue-the-mask');
 
 
 Vue.config.productionTip = false;
