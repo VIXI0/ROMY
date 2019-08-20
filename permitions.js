5bc112c7c94a5b73670ea59a0c40a516f87de7d9
VIXI0 <mickycanela@gmail.com>
2019-08-20 17:51:32 -0400

    permition format

---
+++ permitions.js
@@ -0,0 +1,32 @@
+
+ {nombre: "Developer", permisos: [
+
+    {sistema: "Inventario",link: "/inv_main", menu: [
+
+      {titulo: "Mantenimiento", menu: false, sub: [
+        {tipo: "i", titulo: "Suplidores", link: "/inv_suplidores", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true},
+        {tipo: "i", titulo: "Marcas", link: "/inv_marcas", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true},
+        {tipo: "i", titulo: "Poductos", link: "/inv_productos", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true}
+      ]},
+
+      {titulo: "Proceso", menu: false, sub: [
+        {tipo: "g", titulo: "Entrada", sub:[
+          {tipo: "i", titulo: "Compra", link: "", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true},
+          {tipo: "i", titulo: "Trasferencia", link: "", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true},
+          {tipo: "i", titulo: "Recepción O.C.", link: "", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true}
+        ]},
+        {tipo: "i", titulo: "Orden De Compra", link: "", href:"", target: "_self", c: true, r: true, u: true,d: true, a: true}
+      ]},
+
+      {titulo: "Consulta y Reporte", menu: false, sub: [
+        {tipo: "i", titulo: "Data Credito", href: "https://www.datacredito.com.do/", target: "_blank", c: true, r: true, u: true,d: true, a: true}
+      ]},
+
+      {titulo: "Utilidades", menu: false, sub: []},
+
+      {titulo: "Seguridad", menu: false, sub: []},
+
+      {titulo: "Ayuda", menu: false, sub: []}
+
+    ] }
+  ]}
